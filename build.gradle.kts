838e57a81bad0f3738299bef659fc560d7fc6f08
Namju Kim <cmsong111@naver.com>
2025-03-09 03:28:30 +0900

    Chore: Add Platform Environment

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
+import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
+
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
     id("org.springframework.boot") version "3.4.3"
     id("io.spring.dependency-management") version "1.1.7"
     id("org.graalvm.buildtools.native") version "0.10.5"
+    id("com.gorylenko.gradle-git-properties") version "2.5.0"
 }
 
 group = "io.github.cmsong111"
@@ -20,10 +23,11 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
@@ -39,4 +43,11 @@ kotlin {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+val platform: String = System.getenv("IMAGE_PLATFORM") ?: "linux/amd64"
+
+tasks.withType<BootBuildImage> {
+    createdDate = "now"
+    imagePlatform = platform
 }
