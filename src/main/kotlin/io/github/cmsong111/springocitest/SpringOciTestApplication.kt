6b41a96fc5a228afe30968c5b5050a4f31012dd0
Namju Kim <cmsong111@naver.com>
2025-03-09 03:16:30 +0900

    Initial Commit

---
+++ src/main/kotlin/io/github/cmsong111/springocitest/SpringOciTestApplication.kt
@@ -0,0 +1,11 @@
+package io.github.cmsong111.springocitest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringOciTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringOciTestApplication>(*args)
+}
---
+++ src/test/kotlin/io/github/cmsong111/springocitest/SpringOciTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.cmsong111.springocitest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringOciTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
